f982c95a85b61de0228b69839af7daf65d1b18b9
Rafa <rarg93@hotmail.com>
2019-08-30 17:49:30 -0500

    update keys

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,23 @@
 module.exports = {
     database:{
         host:'https://apipinturas.herokuapp.com/',
-        user:'root',
-        password:'root',
-        database:'database_pinturas',
+        user:'b18469d6706cbb',
+        password:'1dbe4ebd ',
+        database:'heroku_1b819384bce6abd',
         multipleStatements: true,
         connectionLimit : 100,
 
     }
 };
+
+// module.exports = {
+//     database:{
+//         host:'localhost',
+//         user:'root',
+//         password:'root',
+//         database:'database_pinturas',
+//         multipleStatements: true,
+//         connectionLimit : 100,
+
+//     }
+// };
