a4c67089444c3acb7919e9feca3d43ed8bc57d96
Rafa <rarg93@hotmail.com>
2019-08-31 02:51:09 -0500

    heroku

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,8 +17,8 @@ const template = `
 <body>
   <div class="container">
     <h1>WEB NUTRIOLOGA</h1>
-    <p>Esta API fue creada en la materia de Aplicaciones Web Interactivas por Rafael Alejandro Renteria
-      Gomez y Julio Cesar Marin Cardona.</p>
+    <p>Esta API fue creada por Rafael Alejandro Renteria
+      Gomez </p>
     <h2>Servicios</h2>
     <table class="table">
       <tr>
@@ -27,162 +27,39 @@ const template = `
         <th>Descripcion</th>
       </tr>
       <tr>
-        <td>/usuarios</td>
+        <td>/users</td>
         <td>Get</td>
-        <td>Obtiene todos los usuarios registrados</td>
+        <td></td>
       </tr>
       <tr>
-        <td>/usuarios/:id</td>
+        <td>/users/:id</td>
         <td>Get</td>
-        <td>Obtiene el usuario registrado especificado por id</td>
+        <td></td>
       </tr>
       <tr>
-        <td>/usuarios/signup</td>
+        <td>/users/signin</td>
         <td>Post</td>
         <td>Agrega un nuevo usuario</td>
       </tr>
       <tr>
-        <td>/usuarios/login</td>
+        <td>/users/login</td>
         <td>Post</td>
-        <td>Se loggea en el API y genera el token para la autentificación</td>
+        <td></td>
       </tr>
       <tr>
-        <td>/usuarios/:id</td>
+        <td>/users/:id</td>
         <td>Put</td>
-        <td>Modifica usuario especificado por id</td>
+        <td></td>
       </tr>
       <tr>
-        <td>/usuarios/:id</td>
+        <td>/users/:id</td>
         <td>Delete</td>
-        <td>Borra el usuario especificado por id</td>
+        <td></td>
       </tr>
-      <tr>
-        <td>/perfiles</td>
-        <td>Get</td>
-        <td>Obtiene todos los perfiles de los usuarios registrados</td>
-      </tr>
-      <tr>
-        <td>/perfiles/:id</td>
-        <td>Get</td>
-        <td>Obtiene el perfil especificado del usuario por id</td>
-      </tr>
-      <tr>
-        <td>/perfiles</td>
-        <td>Post</td>
-        <td>Agrega un nuevo perfil a un usuario en especifico por id</td>
-      </tr>
-      <tr>
-        <td>/perfiles/:id</td>
-        <td>Put</td>
-        <td>Modifica el perfil del usuario especificado por id</td>
-      </tr>
-      <tr>
-        <td>/perfiles/:id</td>
-        <td>Delete</td>
-        <td>Borra el perfil del usuario especificado por id</td>
-      </tr>
-      <tr>
-        <td>/citas</td>
-        <td>Get</td>
-        <td>Obtiene todas las citas de los usuarios</td>
-      </tr>      
-      <tr>
-        <td>/citas/:id</td>
-        <td>Get</td>
-        <td>Obtiene todas la cita del usuario especificada por id</td>
-      </tr>
-      <tr>
-        <td>/citas</td>
-        <td>Post</td>
-        <td>Agrega una nueva cita al usuario especificado por id</td>
-      </tr>
-      <tr>
-        <td>/citas/:id</td>
-        <td>Put</td>
-        <td>Modifica la cita del usuario especificado por id</td>
-      </tr>
-      <tr>
-        <td>/citas/:id</td>
-        <td>Delete</td>
-        <td>Borra la cita del usuario especificada por id</td>
-      </tr>
-      <tr>
-        <td>/planesAlimenticios</td>
-        <td>Get</td>
-        <td>Obtiene todas los planes alimenticios de los usuarios</td>
-      </tr>
-      <tr>
-        <td>/planesAlimenticios/:id</td>
-        <td>Get</td>
-        <td>Obtiene todas el plan alimenticio por id especificado</td>
-      </tr>
-      <tr>
-        <td>/planesAlimenticios</td>
-        <td>Post</td>
-        <td>Agrega un nuevo plan alimenticio al usuario especificado por id</td>
-      </tr>
-      <tr>
-        <td>/planesAlimenticios/:id</td>
-        <td>Put</td>
-        <td>Modifica el plan alimenticio del usuario especificado por id</td>
-      </tr>
-      <tr>
-        <td>/planesAlimenticios/:id</td>
-        <td>Delete</td>
-        <td>Borra el plan alimenticio del usuario especificado por id</td>
-      </tr>
-      <tr>
-        <td>/raciones</td>
-        <td>Get</td>
-        <td>Obtiene todas las raciones de todos los planes alimenticios</td>
-      </tr>
-      <tr>
-        <td>/raciones/:id</td>
-        <td>Get</td>
-        <td>Obtiene la racion del plan alimeticio especificado por id</td>
-      </tr>
-      <tr>
-        <td>/raciones</td>
-        <td>Post</td>
-        <td>Agrega la racion al plan alimenticio especificado por id</td>
-      </tr>
-      <tr>
-        <td>/raciones/:id</td>
-        <td>Put</td>
-        <td>Modifica las raciones del plan alimenticio especificado por id</td>
-      </tr>
-      <tr>
-        <td>/raciones/:id</td>
-        <td>Delete</td>
-        <td>Borra la racion especificada por id</td>
-      </tr>
-      <tr>
-        <td>/platillosDiarios</td>
-        <td>Get</td>
-        <td>Obtiene todos los platillos diarios</td>
-      </tr>
-      <tr>
-        <td>/platillosDiarios/:id</td>
-        <td>Get</td>
-        <td>Obtiene el platillo diario especificado por id</td>
-      </tr>
-      <tr>
-        <td>/platillosDiarios</td>
-        <td>Post</td>
-        <td>Agrega un nuevo platillo diario</td>
-      </tr>
-      <tr>
-        <td>/platillosDiarios/:id</td>
-        <td>Put</td>
-        <td>Modifica un platillo diario especificado por id</td>
-      </tr>
-      <tr>
-        <td>/platillosDiarios/:id</td>
-        <td>Delete</td>
-        <td>Borra el platillo diario especificado por id</td>
-      </tr>
+     
     </table>
   </div>
+  Postman collection LINK https://www.getpostman.com/collections/252cf368464e82ea1a93
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
