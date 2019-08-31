41c57da75c28fac66210f404529f74e229c77583
Rafa <rarg93@hotmail.com>
2019-08-31 02:41:28 -0500

    heroku

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const checkAuth = require('../middleware/chech-auth')
+const { database } = require('../keys');
 
 var categorieModel = require('../models/categorie');
 
