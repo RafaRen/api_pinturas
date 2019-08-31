202533727bb9576033fc7c8814ad29f89fccc79a
Rafa <rarg93@hotmail.com>
2019-08-30 22:49:46 -0500

    heroku

--- src/database.js
+++ src/database.js
@@ -3,30 +3,14 @@ const mysql = require('mysql');
 const { database } = require('./keys');
 
 const mysqlConnection = mysql.createConnection(database);
-
-
-function handleDisconnect() {
-  console.log('handleDisconnect()');
-  connection.destroy();
-  connection = mysql.createConnection(db_config);
-  connection.connect(function (err) {
+  
+  mysqlConnection.connect(function (err) {
     if (err) {
-      console.log(' Error when connecting to db  (DBERR001):', err);
-      setTimeout(handleDisconnect, 1000);
+      console.error(err);
+      return;
+    } else {
+      console.log('db is connected');
     }
   });
-
-}
-
-mysqlConnection.connect(function (err) {
-  if (err) {
-    console.log('Connection is asleep (time to wake it up): ', err);
-    setTimeout(handleDisconnect, 1000);
-    handleDisconnect();
-    return;
-  } else {
-    console.log('db is connected');
-  }
-});
-
-module.exports = mysqlConnection;
+  
+  module.exports = mysqlConnection;
--- src/keys.js
+++ src/keys.js
@@ -1,24 +1,24 @@
 //Keys for heroku mysql ClearDB MySQL
-// module.exports = {
-//     database:{
-//         host:'us-cdbr-iron-east-02.cleardb.net',
-//         user:'b18469d6706cbb',
-//         password:'1dbe4ebd ',
-//         database:'heroku_1b819384bce6abd',
-//         multipleStatements: true,
-//         connectionLimit : 100
-
-//     }
-// };
-//Local keys
 module.exports = {
     database:{
-        host:'localhost',
-        user:'root',
-        password:'root',
-        database:'database_pinturas',
+        host:'us-cdbr-iron-east-02.cleardb.net',
+        user:'b18469d6706cbb',
+        password:'1dbe4ebd ',
+        database:'heroku_1b819384bce6abd',
         multipleStatements: true,
         connectionLimit : 100
 
     }
 };
+//Local keys
+// module.exports = {
+//     database:{
+//         host:'localhost',
+//         user:'root',
+//         password:'root',
+//         database:'database_pinturas',
+//         multipleStatements: true,
+//         connectionLimit : 100
+
+//     }
+// };
